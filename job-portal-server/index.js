1faccd9f0679eed9d55e455c7c293bc14370ef8e
Byambak <106945976+Byambak@users.noreply.github.com>
2024-04-30 01:50:13 +0800

    it-find job
    backend server

---
+++ job-portal-server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors =require('cors')
+const port = process.env.PORT || 5000;
+
+
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello Developer!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

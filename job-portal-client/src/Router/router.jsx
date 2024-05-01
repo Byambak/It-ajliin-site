f081c2072816065d7c4a0098f49e60651ba7f36b
Byambak <106945976+Byambak@users.noreply.github.com>
2024-05-01 17:01:27 +0800

    it-find job
    Mongo DB

--- job-portal-client/src/Router/router.jsx
+++ job-portal-client/src/Router/router.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Home from "../Pages/Home";
 import { About } from "../Pages/About";
 import CreateJob from "../Pages/CreateJob";
+import Myjobs from "../Pages/Myjobs";
 
 const router = createBrowserRouter([
     {
@@ -13,7 +14,11 @@ const router = createBrowserRouter([
         {
          path:"/post-job",
          element:<CreateJob/>
-        }
+        },
+        {
+          path:"/my-job",
+          element:<Myjobs/>
+        },
         
       ],
     },
